7b35bebf401ac64178d50f345206b785cf12b623
saasbook <saasbook@saasbook.(none)>
2013-01-06 14:14:06 -0800

    initial demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cc1f3cae5b7f3a3da05d14f2126086a50c9759c0be554e765c63ec03cb86371b5534a38341d890798e939f5ec39c5ba111ba377eb6ad2e3bae9c2631afb34e1d'
